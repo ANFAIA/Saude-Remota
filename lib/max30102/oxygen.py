f6aee67a0f5a1a1f71cc79d60618db07e6fbb7b5
irenegallardo31 <irenegallardo31@gmail.com>
2026-06-03 20:54:46 +0200

    Cambios en oxygen.py

--- lib/max30102/oxygen.py
+++ lib/max30102/oxygen.py
@@ -43,7 +43,7 @@
 #  la relación de amplitudes AC/DC de las señales IR y RED.
 class OxygenSaturation:
     FreqS = 50
-    BUFFER_SIZE = FreqS * 8
+    BUFFER_SIZE = FreqS * 4
     MA4_SIZE = 4
 
     SPO2_TABLE = [
